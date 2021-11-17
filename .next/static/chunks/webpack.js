6693294a1d9cf64df4e3e67ac75a5173b3b84ed7
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-11-17 15:30:35 -0300

    configurando _document e fonte externa

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0341a721e7af7cfd"; }
+/******/ 		__webpack_require__.h = function() { return "d1ba7564e0b65855"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
