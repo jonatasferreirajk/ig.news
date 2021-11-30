65f3a0b23a4db71819063dc28473c49a34d0270b
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-11-29 21:14:11 -0300

    Configurando FaunaDB

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d31f360abc52090f"; }
+/******/ 		__webpack_require__.h = function() { return "8096760118325582"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
