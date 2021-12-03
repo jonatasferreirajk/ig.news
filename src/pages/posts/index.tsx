9217a49d1f07a542ce2fc1487d6cb77339051071
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-12-03 17:22:56 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,46 +1,38 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import Prismic from '@prismicio/client';
-import { getPrismicClient } from '../../services/prismic';
-import styles from './styles.module.scss';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
+import styles from "./styles.module.scss";
 
-export default function Posts(){
-  return(
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
+  return (
     <>
       <Head>
         <title>Posts | Ignews</title>
       </Head>
 
       <main className={styles.container}>
-         <div className={styles.posts}>
-           <a href="#">
-             <time>03 of december of 2021</time>
-             <strong>Creating a blog with Next.JS & JAMStack</strong>
-             <p>
-               In this guide, you will learn how to 
-               create a Blog with this technology's.
-             </p>
-           </a>
-         
-           <a href="#">
-             <time>03 of december of 2021</time>
-             <strong>Creating a blog with Next.JS & JAMStack</strong>
-             <p>
-               In this guide, you will learn how to 
-               create a Blog with this technology's.
-             </p>
-           </a>
-         
-           <a href="#">
-             <time>03 of december of 2021</time>
-             <strong>Creating a blog with Next.JS & JAMStack</strong>
-             <p>
-               In this guide, you will learn how to 
-               create a Blog with this technology's.
-             </p>
-           </a>
-
-         </div>
+        <div className={styles.posts}>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+        </div>
       </main>
     </>
   );
@@ -49,15 +41,33 @@ export default function Posts(){
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const response = await prismic.query([
-    Prismic.predicates.at('document.type', 'post')
-  ], {
-    fetch: ['post.title', 'post.content'],
-    pageSize: 100,
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
   });
 
-  console.log(JSON.stringify(response, null, 2));
   return {
-    props: {}
-  }
-}
+    props: { posts },
+  };
+};
