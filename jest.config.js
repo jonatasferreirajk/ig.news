68e9c1b1f4e05418fd6701ffa83088cf703e8306
Jonatas Ferreira <silvajonatas2@gmail.com>
2022-05-23 18:19:36 -0300

    Testando primeiro componente + correções

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: [
+    //"<rootDir>/src/tests/setupTests.ts"
+    '@testing-library/jest-dom/extend-expect'
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  testEnvironment: 'jsdom',
+};
+ 
