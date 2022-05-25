799e39417f7ab3970f2e0f1aac029fd756a9bf44
Jonatas Ferreira <silvajonatas2@gmail.com>
2022-05-24 21:17:39 -0300

    Testando Header + Alteração com Screen

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,13 @@ module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   setupFilesAfterEnv: [
     //"<rootDir>/src/tests/setupTests.ts"
-    '@testing-library/jest-dom/extend-expect'
+    "@testing-library/jest-dom/extend-expect",
   ],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy",
+  },
 };
- 
