6f85b4beefbacc3dbad000f736f5834747b791cc
Jonatas Ferreira <silvajonatas2@gmail.com>
2022-06-08 18:37:40 -0300

    Arquivo que não estava salvo

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -23,7 +23,7 @@ describe("Header component", () => {
   it("renders correctly", () => {
     render(<Header />);
 
-    screen.logTestingPlaygroundURL();
+    // screen.logTestingPlaygroundURL();
 
     expect(screen.getByText("Home")).toBeInTheDocument();
     expect(screen.getByText("Posts")).toBeInTheDocument();
